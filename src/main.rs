75a009e1608face09a2a0b93e109bcef30cac3cc
Daniel R <nzdanron@gmail.com>
2023-05-08 21:57:14 +1200

    Added serialisation module
    Moved all the de/serialisation code into its own module. Tried to make
    the code as versatile as possible, and added function headers.

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,17 @@
 mod account;
+mod serialisation;
 
-use account::Account;
+use serialisation::{deserialise, serialise};
 use magic_crypt::new_magic_crypt;
 
-use std::fs::File;
-use std::io::{
-    prelude::{Read, Write},
-    Result
-};
+const PASSWORD_FILE: &str = "testing.txt";
 
 fn main() {
     let magic_crypt = new_magic_crypt!("thisIsMyPasswordKeyItsSuperLong", 256);
 
-    let accounts = deserialise(&magic_crypt).unwrap();
+    let accounts = deserialise(&magic_crypt, PASSWORD_FILE).expect("Failed to deserialise data");
 
-    let mut file = File::create("testing.txt").unwrap();
+    accounts.iter().for_each(|x| println!("{:#?}", x));
 
-    for account in accounts.iter() {
-        println!("{:#?}", account);
-        writeln!(file, "{}", account.encrypt(&magic_crypt)).expect("Failed to write to file");
-    }
-
-    file.flush().unwrap();
-}
-
-fn deserialise(magic_crypt: &magic_crypt::MagicCrypt256) -> Result<Vec<Account>> {
-    let mut file = File::open("testing.txt")?;
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
-    let mut lines = contents.lines();
-
-    let mut accounts: Vec<Account> = vec![];
-
-    for _ in 0..(lines.clone().count()/3) {
-        let mut username = Some(lines.next().expect("Should be safe to unwrap").to_string());
-        if username.clone().unwrap().is_empty() {
-            username = None;
-        }
-        let mut email = Some(lines.next().expect("Should be safe to unwrap").to_string());
-        if email.clone().unwrap().is_empty() {
-            email = None;
-        }
-        let password = lines.next().expect("Should be safe to unwrap").to_string();
-        accounts.push(Account::new(&magic_crypt, username, email, password));
-    }
-    Ok(accounts)
+    serialise(&magic_crypt, accounts, PASSWORD_FILE).expect("Failed to serialise data");
 }
---
+++ src/serialisation.rs
@@ -0,0 +1,62 @@
+use magic_crypt::MagicCrypt256;
+
+use std::fs::File;
+use std::io::{
+    prelude::{Read, Write},
+    Result,
+};
+
+use crate::account::Account;
+
+/// Deserialises and decrypts the password file and returns a vector of Accounts
+///
+/// # Arguments
+/// * `decrypter`     - The thing to decrypt with
+/// * `password_file` - The name of the file containing the encrypted passwords
+///
+/// # Returns
+/// A result either containing the vector of accounts or an IO error
+pub fn deserialise(decrypter: &MagicCrypt256, password_file: &str) -> Result<Vec<Account>> {
+    let mut file = File::open(password_file)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    let mut lines = contents.lines();
+
+    let mut accounts: Vec<Account> = vec![];
+
+    for _ in 0..(lines.clone().count() / 3) {
+        let mut username = Some(lines.next().expect("Should be safe to unwrap").to_string());
+        if username.clone().unwrap().is_empty() {
+            username = None;
+        }
+        let mut email = Some(lines.next().expect("Should be safe to unwrap").to_string());
+        if email.clone().unwrap().is_empty() {
+            email = None;
+        }
+        let password = lines.next().expect("Should be safe to unwrap").to_string();
+        accounts.push(Account::new(&decrypter, username, email, password));
+    }
+    Ok(accounts)
+}
+
+/// Serialises the given vector of Accounts in an encrypted format
+///
+/// # Arguments
+/// * `encrypter`     - The thing to decrypt with
+/// * `accounts`      - The accounts to serialise
+/// * `password_file` - The name of the file to serialise to
+pub fn serialise(
+    encrypter: &MagicCrypt256,
+    accounts: Vec<Account>,
+    password_file: &str,
+) -> Result<()> {
+    let mut file = File::create(password_file)?;
+
+    for account in accounts {
+        writeln!(file, "{}", account.encrypt(&encrypter))?;
+    }
+
+    file.flush()?;
+
+    Ok(())
+}
