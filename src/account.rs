e7db41899fcf9d35fb98b473626274fbe13f0391
Daniel R <nzdanron@gmail.com>
2023-05-08 21:07:13 +1200

    Built a basic prototype serialising tool
    Basic proof of concept of decrypting a password file, and then
    encrypting it and serialising it

--- src/account.rs
+++ src/account.rs
@@ -1,22 +1,22 @@
 use magic_crypt::{MagicCrypt256, MagicCryptTrait};
 
+#[derive(Debug)]
 pub struct Account {
-    magic_crypt: MagicCrypt256,
-    email: Option<String>,
-    username: Option<String>,
-    password: String,
+   pub email: Option<String>,
+   pub username: Option<String>,
+   pub password: String,
 }
 
 impl Account {
     /// Creates a new Account
     ///
     /// # Arguments
-    /// * `decrypter` - The thing to decrypt and encrypt with
+    /// * `decrypter` - The thing to decrypt with
     /// * `username`  - The encrypted username. Option as the user doesn't have to attach one
     /// * `email`     - The encrypted email. Option as the user doesn't have to attach one
     /// * `password`  - The email attached to the account, option as the user might not enter it
     pub fn new(
-        decrypter: MagicCrypt256,
+        decrypter: &MagicCrypt256,
         username: Option<String>,
         email: Option<String>,
         password: String,
@@ -44,7 +44,6 @@ impl Account {
             .expect("Password failed to decrypt");
 
         Self {
-            magic_crypt: decrypter,
             email,
             username,
             password,
@@ -53,21 +52,24 @@ impl Account {
 
     /// Encrypts the Account into a string with the stored encrypter
     ///
+    /// # Arguments
+    /// * `encrypter` - The thing to decrypt with
+    ///
     /// # Returns
     /// A string with newline breaks for each element
-    pub fn encrypt(&self) -> String {
+    pub fn encrypt(&self, encrypter: &MagicCrypt256) -> String {
         let encrypted_email = match &self.email {
-            Some(email) => self.magic_crypt.encrypt_to_base64(&email),
+            Some(email) => encrypter.encrypt_to_base64(&email),
             None => "".to_string(),
         };
 
         let encrypted_username = match &self.username {
-            Some(username) => self.magic_crypt.encrypt_to_base64(&username),
+            Some(username) => encrypter.encrypt_to_base64(&username),
             None => "".to_string(),
         };
 
-        let encrypted_password = self.magic_crypt.encrypt_to_base64(&self.password);
+        let encrypted_password = encrypter.encrypt_to_base64(&self.password);
 
-        format!("{}\n{}\n{}\n", encrypted_username, encrypted_email, encrypted_password)
+        format!("{}\n{}\n{}", encrypted_username, encrypted_email, encrypted_password)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+mod account;
+
+use account::Account;
+use magic_crypt::new_magic_crypt;
+
+use std::fs::File;
+use std::io::prelude::{Read, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let magic_crypt = new_magic_crypt!("thisIsMyPasswordKeyItsSuperLong", 256);
+
+    let mut file = File::open("testing.txt").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    let mut lines = contents.lines();
+
+    let mut accounts: Vec<Account> = vec![];
+
+    for _ in 0..(lines.clone().count()/3) {
+        let username = lines.next().expect("Should be safe to unwrap").to_string();
+        let email = lines.next().expect("Should be safe to unwrap").to_string();
+        let password = lines.next().expect("Should be safe to unwrap").to_string();
+        accounts.push(Account::new(&magic_crypt, Some(username), Some(email), password));
+    }
+
+    let mut file = File::create("testing.txt").unwrap();
+
+    for account in accounts.iter() {
+        println!("{:#?}", account);
+        writeln!(file, "{}", account.encrypt(&magic_crypt)).expect("Failed to write to file");
+    }
+
+    file.flush().unwrap();
 }
