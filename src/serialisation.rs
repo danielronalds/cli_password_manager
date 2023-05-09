5f60bf67fabdd77d70159840cdac08553924fa82
Daniel R <nzdanron@gmail.com>
2023-05-10 10:16:33 +1200

    Formatted and updated documentation of the serialisation module

--- src/serialisation.rs
+++ src/serialisation.rs
@@ -1,3 +1,22 @@
+//! This module contains the code for reading and writing the stored passwords in the manager to
+//! the user's local storage.
+//!
+//! # File Format
+//!
+//! 1. file_password
+//! 2. account_one_label
+//! 3. account_one_username
+//! 4. account_one_email
+//! 5. account_one_password
+//! 6. account_two_label
+//! 7. account_two_username
+//! 8. account_two_email
+//! 9. account_two_password
+//!
+//! etc...
+//!
+//! Accounts that do not have an email or a username have blank lines instead
+
 use magic_crypt::{MagicCrypt256, MagicCryptTrait};
 
 use std::fs::File;
@@ -11,12 +30,19 @@ use crate::account::Account;
 /// Deserialises and decrypts the password file and returns a vector of Accounts
 ///
 /// # Arguments
+///
 /// * `decrypter`     - The thing to decrypt with
 /// * `password_file` - The name of the file containing the encrypted passwords
+/// * `password`      - The password that the user has entered to login
 ///
 /// # Returns
+///
 /// A result either containing the vector of accounts or an IO error
-pub fn deserialise(decrypter: &MagicCrypt256, password_file: &str, password: &str) -> Result<Vec<Account>> {
+pub fn deserialise(
+    decrypter: &MagicCrypt256,
+    password_file: &str,
+    password: &str,
+) -> Result<Vec<Account>> {
     let mut file = File::open(password_file)?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
@@ -24,12 +50,13 @@ pub fn deserialise(decrypter: &MagicCrypt256, password_file: &str, password: &st
 
     let mut accounts: Vec<Account> = vec![];
 
-    let passkey = decrypter.decrypt_base64_to_string(lines.next().expect("Should be safe to unwrap")).unwrap();
+    let passkey = decrypter
+        .decrypt_base64_to_string(lines.next().expect("Should be safe to unwrap"))
+        .unwrap();
 
-    if password !=  passkey {
+    if password != passkey {
         println!("{}", passkey);
         panic!("WRONG PASSWORD!");
-
     }
 
     for _ in 0..(lines.clone().count() / 4) {
@@ -57,18 +84,21 @@ pub fn deserialise(decrypter: &MagicCrypt256, password_file: &str, password: &st
 /// Serialises the given vector of Accounts in an encrypted format
 ///
 /// # Arguments
+///
 /// * `encrypter`     - The thing to decrypt with
 /// * `accounts`      - The accounts to serialise
 /// * `password_file` - The name of the file to serialise to
+/// * `password`      - The user's password to verify against
 pub fn serialise(
     encrypter: &MagicCrypt256,
     accounts: Vec<Account>,
     password_file: &str,
-    password: &str
+    password: &str,
 ) -> Result<()> {
     let mut file = File::create(password_file)?;
 
-    writeln!(file, "{}", encrypter.encrypt_str_to_base64(password)).expect("Failed to write passkey");
+    writeln!(file, "{}", encrypter.encrypt_str_to_base64(password))
+        .expect("Failed to write passkey");
 
     for account in accounts {
         writeln!(file, "{}", account.encrypt(&encrypter))?;
